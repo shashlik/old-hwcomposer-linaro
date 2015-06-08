caadd71c74706ebcb5569a9f2d6e06dba5bdbf96
xinliang.liu <xinliang.liu@linaro.org>
2015-06-08 19:38:53 +0800

    remove libkms from LOCAL_SHARED_LIBRARIES Change to LOCAL_MODULE_RELATIVE_PATH
    Signed-off-by: xinliang.liu <xinliang.liu@linaro.org>

--- Android.mk
+++ Android.mk
@@ -19,11 +19,11 @@ LOCAL_PATH := $(call my-dir)
 # hw/<OVERLAY_HARDWARE_MODULE_ID>.<ro.product.board>.so
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_SHARED_LIBRARIES := liblog libdrm libkms libhardware libsync libcutils
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_SHARED_LIBRARIES := liblog libdrm libhardware libsync
 #libion
 LOCAL_SRC_FILES := hwcomposer.cpp
-LOCAL_MODULE := hwcomposer.default
+LOCAL_MODULE := hwcomposer.$(TARGET_BOARD_PLATFORM)
 LOCAL_CFLAGS:= -DLOG_TAG=\"hwcomposer\"
 LOCAL_MODULE_TAGS := eng
 LOCAL_C_INCLUDES += \
